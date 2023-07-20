3d3e3fb694076afe3d71b3c309109354fb57928e
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-07-20 14:32:43 +0300

    assignment Done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
+
 
 function Item({ name, category }) {
+  const[cart,setToCart]=useState(false)
+
+  const addCart= ()=>{
+    cart? setToCart(false):setToCart(true)
+  }
+
+   let value = cart ? "in-cart": ""
+   var text = cart ? "Remove from Cart" :"Add to Cart"
+   
   return (
-    <li className="">
+    <li className={value}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addCart}>{text}</button>
     </li>
   );
 }
